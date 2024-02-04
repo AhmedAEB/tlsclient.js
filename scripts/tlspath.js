0e7b9841d0a984b2920a625e652c199101e9a4f7
AEB <50132811+AhmedAEB@users.noreply.github.com>
2024-02-04 18:25:30 +0000

    Reversed

--- scripts/tlspath.js
+++ scripts/tlspath.js
@@ -18,19 +18,18 @@ if (platform === "win32") {
   filename = "tls-client-linux";
   extension = "so";
 
-  let releaseDetails = fs.readFileSync("/etc/os-release", "utf8");
+  let releaseDetails = readFileSync("/etc/os-release", "utf8");
   const lines = releaseDetails.split("\n");
   const release = {};
-  lines.forEach((line) => {
+  lines.forEach((line, _) => {
+    // Split the line into an array of words delimited by '='
     const words = line.split("=");
-    if (words.length >= 2) {
-      release[words[0].trim().toLowerCase()] = words[1].trim();
-    }
+    release[words[0].trim().toLowerCase()] = words[1].trim();
   });
 
-  if (release.id && release.id.toLowerCase().includes("ubuntu")) {
+  if (release.id.toLowerCase().includes("ubuntu")) {
     distribution = "ubuntu-amd64";
-  } else if (release.id && release.id.toLowerCase().includes("alpine")) {
+  } else if (release.id.toLowerCase().includes("alpine")) {
     distribution = `alpine-amd64`;
   } else {
     distribution = arch == "arm64" ? arch : "armv7";
@@ -39,7 +38,6 @@ if (platform === "win32") {
   console.error(`Unsupported platform: ${platform}`);
   process.exit(1);
 }
-
 let _filename = `${filename}-${distribution}-v${version}.${extension}`;
 const url = `https://github.com/bogdanfinn/tls-client/releases/download/v1.7.2/${_filename}`;
 const destination = `${os.tmpdir()}/${_filename}`;
